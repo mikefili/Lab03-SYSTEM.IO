c40a3e887ae34cfb88c05c3b9ea72b6445dfe71a
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-13 21:00:57 -0800

    GAME WORKS!!!! Need to refactor a few features and comment code out

--- Lab03-SYSTEMIO/Lab03-SYSTEMIO/Program.cs
+++ Lab03-SYSTEMIO/Lab03-SYSTEMIO/Program.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace Lab03_SYSTEMIO
 {
     class Program
     {
-        private static string newWord;
+        public static string newWord;
+        public static string mysteryWord;
+        public static bool correct = false;
         public static string path = ("../../../wordBank.txt");
-        string[] words = { "dog", "puppy", "pooch", "pupper", "woofer" };
+        public static string attempts = ("../../../attempts.txt");
 
         static void Main(string[] args)
         {
+            CreateWordList(path);
             bool runHomeNav = true;
             while (runHomeNav)
             {
@@ -42,8 +46,7 @@ namespace Lab03_SYSTEMIO
             {
                 // if user selects '1'
                 case "1":
-                    Console.WriteLine("OPTION 1");
-                    Console.ReadLine();
+                    PlayGame(mysteryWord);
                     break;
 
                 // if user selects '2' 
@@ -59,8 +62,71 @@ namespace Lab03_SYSTEMIO
             }
         }
 
-        static void PlayGame(string path)
+        public static void PlayGame(string mysteryWord)
         {
+            Console.Clear();
+            mysteryWord = RandomWord(path);
+            //File.Create(attempts).Dispose();
+            string[] gameBoard = new string[mysteryWord.Length];
+            for (int i = 0; i < mysteryWord.Length; i++)
+            {
+                gameBoard[i] = "___ ";
+                Console.Write($"{gameBoard[i]}");
+            }
+            Console.WriteLine("please enter a letter");
+            while (gameBoard.Contains("___ "))
+            {
+                string guessedLetter = Console.ReadLine();
+                if (guessedLetter.Length != 1)
+                {
+                    Console.WriteLine("please press one letter");
+                }
+                bool result = GuessValidator(mysteryWord, guessedLetter);
+                if (result == true)
+                {
+                    for (int i = 0; i < mysteryWord.Length; i++)
+                    {
+                        if (mysteryWord[i].ToString() == guessedLetter)
+                        {
+                            int guessIndex = mysteryWord.IndexOf(guessedLetter);
+                            gameBoard[i] = mysteryWord[i].ToString();
+                        }
+                    }
+                }
+                try
+                {
+                    using (StreamWriter streamWriter = File.AppendText(attempts))
+                    {
+                        streamWriter.WriteLine(guessedLetter);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error occurred: '{0}'", e);
+                }
+                for (int i = 0; i < gameBoard.Length; i++)
+                {
+                    Console.Write($"{gameBoard[i]}");
+                }
+                Console.Write("    ");
+                string[] lines = File.ReadAllLines(attempts);
+                Console.WriteLine("Guessed: ");
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    Console.WriteLine(lines[i]);
+                }
+            }
+            Console.WriteLine("       ___.__. ____  __ __ ");
+            Console.WriteLine("      <   |  |/  _ \\|  |  \\");
+            Console.WriteLine("       \\___  (  <_> )  |  /");
+            Console.WriteLine("       / ____|\\____/|____/");
+            Console.WriteLine();
+            Console.WriteLine("              .__");
+            Console.WriteLine("      __  _  _|__| ____");
+            Console.WriteLine("      \\ \\/ \\/ /  |/    \\");
+            Console.WriteLine("       \\     /|  |   |  \\");
+            Console.WriteLine("        \\/\\_/ |__|___|  /");
+            Console.WriteLine("                      \\/");
 
         }
 
@@ -68,8 +134,69 @@ namespace Lab03_SYSTEMIO
         {
             Random random = new Random();
             string[] words = ReadFile(path);
-            int selectedIndex = random.Next(words.Length);
-            return words[selectedIndex];
+            int randomIndex = random.Next(words.Length);
+            return words[randomIndex];
+        }
+
+        public static bool GuessValidator(string mysteryWord, string guessedLetter)
+        {
+            string temp = "";
+            for (int i = 0; i < mysteryWord.Length; i++)
+            {
+                if (mysteryWord[i].ToString() == guessedLetter)
+                {
+                    temp = temp + guessedLetter;
+                }
+            }
+            if (temp != "") return true;
+            else return false;
+        }
+
+        public static string[] UpdateFile(string path, string input)
+        {
+            string formattedInput = input.ToUpper();
+
+            using (StreamWriter streamWriter = File.AppendText(path))
+            {
+                streamWriter.WriteLine(formattedInput);
+            }
+            return ReadFile(path);
+        }
+
+        public static char[] WordValidator(string word)
+        {
+            int wordLength = word.Length;
+            char[] character = new char[wordLength];
+            for (int i = 0; i < wordLength; i++)
+            {
+                character[i] = '_';
+            }
+
+            string[] attemptedLetters = File.ReadAllLines(attempts);
+            char[] attemptedChar = new char[attemptedLetters.Length];
+            for (int i = 0; i < attemptedLetters.Length; i++)
+            {
+                attemptedChar[i - 1] = Convert.ToChar(attemptedLetters[i]);
+            };
+            for (int i = 0; i < attemptedLetters.Length; i++)
+            {
+                for (int j = 0; j < word.Length; j++)
+                {
+                    if (word[j] == attemptedChar[i])
+                    {
+                        character[j] = word[j];
+                    }
+                }
+            }
+            return character;
+        }
+
+        public static string TakeInGuess()
+        {
+            Console.Write("Guess a Letter: ");
+            string guessedLetterRaw = Console.ReadLine();
+            string guessedLetter = guessedLetterRaw.ToUpper();
+            return guessedLetter;
         }
 
         static void AdminMenu()
@@ -105,6 +232,7 @@ namespace Lab03_SYSTEMIO
                     break;
 
                 case "3":
+                    Console.Clear();
                     RemoveWord(path);
                     break;
 
@@ -112,6 +240,33 @@ namespace Lab03_SYSTEMIO
                     Console.Clear();
                     HomeNav();
                     break;
+            }
+        }
+
+        public static void CreateWordList(string path)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(path))
+            {
+                string[] words = { "dog", "puppy", "pooch", "pupper", "woofer" };
+                try
+                {
+                    foreach (string word in words)
+                    {
+                        streamWriter.WriteLine($"{word}");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error occurred: '{0}'", e);
+                }
+            }
+        }
+
+        public static void CreateGuessList()
+        {
+            using (StreamWriter streamWriter = new StreamWriter(attempts))
+            {
+                streamWriter.WriteLine("Your Attempts: ");
             }
         }
 
@@ -192,10 +347,9 @@ namespace Lab03_SYSTEMIO
             {
                 File.Delete(path);
             }
-            catch (Exception)
+            catch (Exception e)
             {
-
-                throw;
+                Console.WriteLine("An error occurred: '{0}'", e);
             }
         }
 
